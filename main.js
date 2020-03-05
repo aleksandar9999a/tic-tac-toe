2ea51a63c947f722a05c53aef3e3a1e14f7819eb
aleksandar9999a <aleksandar9999a@gmail.com>
2020-03-05 16:13:33 +0200

    Added createElement, createTd, createTr, handleClick, genarateTable, etc

--- main.js
+++ main.js
@@ -1,16 +1,74 @@
 class Main {
     conteiner;
+    document;
+    table;
+    lastClick = "O";
 
-    constructor(document) {
-        this.conteiner = document;
+    constructor(conteiner, document) {
+        this.conteiner = conteiner;
+        this.document = document;
+        this.table = this.genarateTable(3, 3);
+    }
+
+    createElement(type, content) {
+        let e = this.document.createElement(type);
+
+        if (typeof content === "string") {
+            e.innerHTML = content;
+        }
+        if (typeof content === "object") {
+            e.appendChild(content);
+        }
+        return e;
+    }
+
+    createTd() {
+        return this.createElement('td');
+    }
+
+    createTr() {
+        return this.createElement('tr');
+    }
+
+    createTable() {
+        return this.createElement('table');
+    }
+
+    handleClick(e) {
+        if (e.target.innerHTML === '') {
+            if (this.lastClick === 'O') {
+                this.lastClick = 'X';
+            } else {
+                this.lastClick = 'O';
+            }
+            
+            return e.target.innerHTML = this.lastClick;
+        }
+        return e.target.innerHTML;
+    }
+
+    genarateTable(rows, cols) {
+        const table = this.createTable();
+        for (let i = 0; i < rows; i++) {
+            const tr = this.createTr();
+            for (let x = 0; x < cols; x++) {
+                const td = this.createTd();
+                td.addEventListener('click', this.handleClick.bind(this));
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+
+        return table;
     }
 
     render() {
-        return  this.conteiner.innerHTML = "Ti si qk"
+        return this.conteiner.appendChild(this.table);
     }
 }
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    const app = document.getElementById('app');
-    new Main(app).render();
+document.addEventListener('DOMContentLoaded', () => {
+    const conteiner = document.getElementById('app');
+    const app = new Main(conteiner, document);
+    app.render();
 })
